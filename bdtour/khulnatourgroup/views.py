c222408db397abfbf3a16b3eb4bf0ea2ee423d8b
Nokibul Amin Mezba <csmezba@gmail.com>
2025-11-10 19:24:18 +0600

    - add static assets for display page

--- bdtour/khulnatourgroup/views.py
+++ bdtour/khulnatourgroup/views.py
@@ -4,9 +4,12 @@ from .models import TourPackage, UserInfo
 # Create your views here.
 
 
+# def index(request):
+#     tourPackage = TourPackage.objects.all()  # pylint: disable=no-member
+#     context = {
+#         'tourPackages': tourPackage
+#     }
+#     return render(request, 'tours/index.html', context)
+
 def index(request):
-    tourPackage = TourPackage.objects.all()  # pylint: disable=no-member
-    context = {
-        'tourPackages': tourPackage
-    }
-    return render(request, 'tours/index.html', context)
+    return render(request, 'django_app/index.html')
